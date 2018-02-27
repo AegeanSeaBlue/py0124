61be367c9b441f7850c96a2a19c6ff1bcd44aece
lei.guo <lei.guo@way.io>
2018-02-27 11:39:50 +0800

    py3

--- t1802261.py
+++ t1802261.py
@@ -6,5 +6,6 @@ descDirc = 'D:/practice/py0124/testf/'
 path, name = os.path.split(srcf)
 descFile = descDirc + name
 copy = shutil.copyfile(srcf, descFile)
-
-print(copy)
+size=os.path.getsize(copy)
+size = int(size/1024)
+print(size)
